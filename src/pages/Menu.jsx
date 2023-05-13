a4e05dc742429021b2951c580c92b4b32f550501
Aleksandr Gubenko <asd@Aleksandrs-MacBook-Pro.local>
2023-05-13 13:11:57 +0300

    small fixes

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <nav>
-        <Link to='/'>Main</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contacts'>Contacts</Link>
-        <Link to='/courses'>Courses</Link>
+      <Link to='/'>Main</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contacts</Link>
     </nav>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
